fbbeff666214363b621ef8664c4022abeb33fee8
Tim Lu <timothymt.lu@gmail.com>
2026-03-23 14:37:46 +1100

    Basic features added - User provides 3 inputs: loan amount, loan period (years), and interest rate. Single output: Monthly repayments

---
+++ project-mortgage-calculator.py
@@ -0,0 +1,76 @@
+"""
+Basic function of the mortgage calculator - User provides 3 inputs: loan amount, loan period (years), and interest rate.
+Single output: Monthly repayments
+
+Future features to be added:
+- Monthly amortisation breaking down Months, Interest this payment, Principal this payment, Interest to date, Principal
+to date, Principal remaining
+- Additional input: Extra repayments/month
+- Additional input: Repayment intervals options - fortnightly/monthly
+"""
+
+"""
+Class objects for interest and principal?
+"""
+
+
+"""
+Function to request the loan amount and returns the same value
+"""
+def req_loan_amount():
+    amount = 0  # Initialise variable to start while loop
+    while amount == 0:
+        try:
+            amount = int(input("Loan Amount: $"))
+        except ValueError:
+            print("Error: Loan Amount must be an integer!")
+        else:
+            return amount
+
+"""
+Function to request the loan period and returns the same value
+"""
+def req_loan_period():
+    period = 0  # Initialise variable to start while loop
+    while period == 0:
+        try:
+            period = int(input("Loan period (years): "))
+        except ValueError:
+            print("Error: Loan Period must be an integer!")
+        else:
+            return period
+
+"""
+Function to request the interest rate and returns the same value
+"""
+def req_interest_rate():
+    interest = 0  # Initialise variable to start while loop
+    while interest == 0:
+        try:
+            interest = float(input("Interest Rate (% p.a.): "))
+        except ValueError:
+            print("Error: Interest Rate must be a floating point!")
+        else:
+            return interest / 100   # Convert from %
+
+"""
+Function to calculate the monthly repayments
+"""
+def calc_repayments(principal,period_y,interest_y):
+    # normalise the values from annually to monthly
+    period_m = period_y * 12
+    interest_m = interest_y / 12
+
+    # Formula for monthly replayments
+    repayment_m = round((principal * interest_m * (1 + interest_m) ** period_m) / ((1 + interest_m) ** period_m - 1),2)
+    return repayment_m
+
+"""
+Main
+"""
+loan_amount = req_loan_amount()
+loan_period = req_loan_period()
+interest_rate = req_interest_rate()
+monthly_repayments = calc_repayments(loan_amount,loan_period,interest_rate)
+print(f"Monthly repayment: ${monthly_repayments}")
+
